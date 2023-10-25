956078e60a90f0b37ba70852baa5a24510bd3800
vinisi12363 <viniciuspv.si@gmail.com>
2023-10-25 04:08:21 -0300

    feat: meny things are added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
+import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/button";
 
 
@@ -14,11 +15,16 @@ export default function Home() {
 			<div className="inline-block max-w-lg text-center justify-center">
 				<h1 className={title()}>My&nbsp;</h1>
 				
-				<h1 className={title({ color: "green" })}>showcase&nbsp;</h1>
+				<h1  className={title({ color: "green", class:"bg-cliptext text-transparent bg-gradient-to-b" })}>Hello There! </h1>
+				<div>
+				<Image
+				 	width={300}
+				 	alt="eu"
+					//TODO ADICIONAR A MINHA LINDA FOTO AQUI
+				
+				/>
+				</div>
 			
-				<h1 className={title()}>
-					will building here.
-				</h1>
 				
 			</div>
 
