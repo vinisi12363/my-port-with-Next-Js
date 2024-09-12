c1d9648f0935d919e4f20f7ffb496811193cb62d
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2024-09-12 13:48:51 -0300

    refactor:  reftoring about texts

--- app/about/content.tsx
+++ app/about/content.tsx
@@ -9,19 +9,13 @@ export const Content = () => (
         Universidade Estadual do Sudoeste da Bahia (UESB).</p>
       <br></br>
         <p className={subtitle()}>
-        Estou no ramo do desenvolvimento web faz 1 ano e meio, estou sempre
+        Estou no ramo do desenvolvimento web há cerca de 2 anos e meio, estou sempre
         buscando aprender mais e me aprimorar nas ferramentas que já utilizo
         diariamente.
       
         Sou naturalmente proativo, extrovertido e bastante comunicativo. 
       
-        Possuo experiência com trabalho em equipes de desenvolvimento que
-        utilizaram de tecnologias de denvolvimento ágil como Scrum e Kanban.
-      
-        Como principal diferencial, posso dizer que sou muito bom em me adaptar
-        ao que é novo, essa capacidade de análise e adaptação ao cenário atual,
-        tem me permitido avançar bastante em minha jornada como profissional da
-        área de desenvolvimento.
+        Comecei na área da programação em 2013 através do Bacharelado em Sistemas de Informação pela Universidade Estadual do Sudoeste da Bahia, curso ao qual me graduei até o sétimo período. Depois de uma pausa na carreira, retornei à área de T.I. Desde então tenho desenvolvido projetos web para empresas da minha cidade. Recentemente, atuei como desenvolvedor Grupo Sideral em um projeto SAAS voltado para a Web e mobile com Laravel e PHP. Além disso, possuo experiência consolidada com ReactJS , Redux, Styled-components e banco de dados, experiência oriunda de projetos e freelancers. Utilizo o JIRA e Github para organizar e versionar tudo que é desenvolvido.
         </p>
       
     </div>
