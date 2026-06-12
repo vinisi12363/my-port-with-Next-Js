4179eb5931e0c31c4c1d8862520c657cd9ddcf63
vinigrowscale <marcos.vinicius@growscale.com.br>
2026-06-12 15:59:18 -0300

    Gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import Script from "next/script";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+
+const GTM_ID = "GTM-N3XXPX6X";
 
 export const metadata: Metadata = {
 	title: {
@@ -34,13 +37,33 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="pt-br" suppressHydrationWarning>
-			<head />
+			<head>
+				{/* Google Tag Manager */}
+				<Script id="gtm" strategy="afterInteractive">
+					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+				</Script>
+				{/* End Google Tag Manager */}
+			</head>
 			<body
 				className={clsx(
 					"min-h-screen font-sans antialiased",
 					fontSans.variable
 				)}
 			>
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe
+						src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
+				{/* End Google Tag Manager (noscript) */}
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
